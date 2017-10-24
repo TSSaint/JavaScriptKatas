343c51cb394a0f6c19ceab13826cee2980bfdcb9
Thomas Santos <thomas@socialeffort.co>
2017-10-24 04:22:25 -0400

    add objs_and_est6.js practice

---
+++ objs_and_es6.js
@@ -0,0 +1,8 @@
+// some practice on es6 template literals in JavaScript
+function colorDesc(color, adj){
+  console.log(`${color} is a really ${adj} color!`);
+}
+
+colorDesc('Black', 'awesome'); // Black is a really awesome color!
+
+
